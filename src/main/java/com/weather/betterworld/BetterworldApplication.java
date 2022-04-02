8699d201331ea14715b3f87c1765f3b4a2e4430d
leeseonseonje <leeseonje9323@gmail.com>
2022-04-02 12:24:38 +0900

    generate Project

---
+++ src/main/java/com/weather/betterworld/BetterworldApplication.java
@@ -0,0 +1,13 @@
+package com.weather.betterworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterworldApplication.class, args);
+	}
+
+}
